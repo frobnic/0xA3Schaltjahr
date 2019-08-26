8ca36b528ca891270bb2fd55ea3eb03561f1a791
frank paulsen <frobnic+ita@gmail.com>
2019-08-26 10:32:46 +0200

    inserted code

--- 0xA3Schaltjahr/Program.cs
+++ 0xA3Schaltjahr/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _0xA3Schaltjahr
 {
@@ -10,6 +6,22 @@ namespace _0xA3Schaltjahr
     {
         static void Main(string[] args)
         {
+            int j;
+
+            Console.WriteLine("Bitte geben sie ein Jahr ein");
+            if (int.TryParse(Console.ReadLine(), out j))
+            {
+                Console.WriteLine("");
+                if (j % 4 != 0)
+                    Console.Write("Kein ");
+                else
+                {
+                    if (j % 100 == 0)
+                        if (j % 400 != 0)
+                            Console.Write("Kein ");
+                }
+                Console.WriteLine("Schaltjahr");
+            }
         }
     }
 }
